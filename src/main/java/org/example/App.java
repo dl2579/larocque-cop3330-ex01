96b891225e1c9bf18982071acf32b3c6df20ae3d
dl2579 <damianlarocque257@gmail.com>
2021-09-09 20:57:26 -0400

    ex1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Damian LaRocque
  */
-public class App 
+public class App //A public class must be placed in a file that has a filename of the form ClassName.java
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World test!" );
+
+        System.out.println( "What is your name?" );
+
+        String name;
+
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+
+        System.out.printf("Hello, %s, nice to meet you!%n", name);
+
+
     }
 }
